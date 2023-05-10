c90df8e4cc9ac64bf124af58f57ada4154327ffb
ShadeAJ1 <41494065+ShadeAJ1@users.noreply.github.com>
2023-05-10 15:59:28 -0700

    acc

--- components/Activities.tsx
+++ components/Activities.tsx
@@ -4,7 +4,7 @@ import { Box, Center, Heading, Text, Stack, Avatar, useColorModeValue, Image, Si
 export default function Activities() {
   const academics = [
     {
-      title: "Robotics Captain, Programmer, and Builder",
+      title: "Robotics Captain, Programmer, & Builder",
       institution: "VEX Robotics @ CHS",
       date: "2019-Present",
       description: ["Apply and expand expertise in C++, embedded systems, robot design and development, and physics", "Demonstrate skills in leadership, collaboration, and communication", "Build a robot capable of competing and completing an objective", "Use vision, inertial, and tracking algorithms to train the robot for autonomous movement", "Compete against other robotics teams at a high level in local, state, and international competitions"],
@@ -14,14 +14,14 @@ export default function Activities() {
       title: "AI4ALL Student",
       institution: "Carngie Mellon University",
       date: "2021",
-      description: ["Attended free research program at CMU with a cohort of 25 students", "Worked with Carnegie Mellon AI professors and researchers to explore AI", "Learnt and used Python libraries such as Tensorflow, Matplotlib, Scikit-learn, and Numpy", "Gained valuable research experience from building AI models and working on projects with other students"],
+      description: ["Attended free program at CMU with a cohort of 25 students", "Worked with Carnegie Mellon AI faculty to explore AI", "Used tools such as Tensorflow, PyTorch, and OpenCV", "Gained valuable research experience from building AI models and working on projects with other students"],
       imageUrl: "/CarnegieMellon.png",
     },
     {
       title: "AI Scholar and Ambassador",
       institution: "Inspirit AI",
       date: "2021",
-      description: ["Worked with Stanford graduates to learn more about AI", "Created AI projects using Python and Google ML tools under experts", "Explored fundamental AI concepts such as regression, NNs, and decision trees ", "Applied AI to use cases in healthcare, genomics, autonomous driving, and more", "Worked as an ambassador to teach AI concepts to students at my school"],
+      description: ["Worked with graduate students to learn more about AI", "Built projects using Python and Google ML tools under experts", "Explored fundamental AI concepts such as regression and NNs", "Applied AI to use cases in healthcare, self-driving, and more", "Worked as an ambassador to teach AI concepts to other students"],
       imageUrl: "/InspiritAI.jpeg",
     },
     // {
@@ -40,8 +40,8 @@ export default function Activities() {
     {
       title: "Founder and President",
       institution: "ByteClub",
-      date: "2020-Present",
-      description: ["Founded organization to increase STEM involvement by teaching students computer science", "Taught through collaborating on projects such as apps, websites, and games", "Pair students that are new to CS with more experienced students to run a peer-peer program", "Raised funds for the organization through donations and fundraisers", "Share resources with students to help them pursue CS further"],
+      date: "2020-23",
+      description: ["Founded org to increase STEM involvement by teaching students CS", "Taught through working on projects such as apps, websites, & games", "Pair students across experience levels to run a peer-peer program", "Share resources with students to help them pursue CS further"],
       imageUrl: "/ByteClub.png",
     },
   ];
