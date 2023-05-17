873db6a5d02428486b32f36ff87ad57e6ea932c8
ShadeAJ1 <41494065+ShadeAJ1@users.noreply.github.com>
2023-05-16 20:57:35 -0700

    nosearch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/images.css";
 import { NextSeo } from "next-seo";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <NextSeo title="Aryan Jain - Calabasas High School" titleTemplate="Aryan Jain" defaultTitle="Aryan Jain - Calabasas High School" description="Passionate about AI, Robotics, and Entrepreneurship. Experienced in Cloud and Web Development. Always building something new." canonical="https://www.aryanj.tech/" />
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      {/* <NextSeo title="Aryan Jain - Calabasas High School" titleTemplate="Aryan Jain" defaultTitle="Aryan Jain - Calabasas High School" description="Passionate about AI, Robotics, and Entrepreneurship. Experienced in Cloud and Web Development. Always building something new." canonical="https://www.aryanj.tech/" /> */}
       <Component {...pageProps} />
     </ChakraProvider>
   );
