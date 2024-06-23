281be1615ce30caef4f8d2a6b047f388199bb336
ShadeAJ1 <41494065+ShadeAJ1@users.noreply.github.com>
2024-06-22 17:59:37 -0700

    added ids

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,146 +1,130 @@
-import { Box,
-    Center,
-    Modal,
-    ModalOverlay,
-    Flex,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Heading,
-    Text,
-    Badge,
-    Stack,
-    Avatar,
-    useColorModeValue,
-    Image,
-    SimpleGrid,
-    ListItem,
-    useDisclosure,
-    List,
-    UnorderedList, 
-    HStack,
-    chakra, 
-    Spacer} from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
-import projects from './projects.json'
-import IMG from 'next/image'
-import { ChakraNextImage } from './ChakraImg'
+import { Box, Center, Modal, ModalOverlay, Flex, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Heading, Text, Badge, Stack, Avatar, useColorModeValue, Image, SimpleGrid, ListItem, useDisclosure, List, UnorderedList, HStack, chakra, Spacer } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import projects from "./projects.json";
+import IMG from "next/image";
+import { ChakraNextImage } from "./ChakraImg";
 
 export default function Projects() {
-
   return (
     <Box px={[5, 5, 10]} py={5}>
-        <Heading fontSize={'2xl'} mb={5}>Projects</Heading>
-        <Text fontSize={'lg'} pt={0} mb={4} fontWeight={500} id="robotics">Robotics</Text>
-            <SimpleGrid columns={[1, 1, 2]} spacing={4}>
-                {projects.robotics.map(({ name, year, description, images, imageDescriptions }) => (
-                    <Box key={name} h='100%'>
-                        <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions}></Project>
-                    </Box>
-                ))}
-            </SimpleGrid>
-        <Text fontSize={'lg'} pt={5} mb={4} fontWeight={500} id="ai">Artificial Intelligence</Text>
-        <SimpleGrid columns={[1, 1, 2]} spacing={4}>
-                {projects.ai.map(({ name, year, description, images, imageDescriptions }) => (
-                    <Box key={name} h='100%'>
-                        <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions}></Project>
-                    </Box>
-                ))}
-            </SimpleGrid>
-        <Text fontSize={'lg'} pt={5} mb={4} fontWeight={500}>Software Development</Text>
-        <SimpleGrid columns={[1, 1, 2]} spacing={4}>
-                {projects.software.map(({ name, year, description, images, imageDescriptions, links }) => (
-                    <Box key={name} h='100%'>
-                        <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions} links={links}></Project>
-                    </Box>
-                ))}
-            </SimpleGrid>
-        <Text fontSize={'lg'} pt={5} mb={4} fontWeight={500}>Cloud Development and More</Text>
-        <SimpleGrid columns={[1, 1, 2]} spacing={4}>
-                {projects.cloud.map(({ name, year, description, images, imageDescriptions, links }) => (
-                    <Box key={name} h='100%'>
-                        <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions} links={links}></Project>
-                    </Box>
-                ))}
-            </SimpleGrid>
-   </Box>
-  )
+      <Heading fontSize={"2xl"} mb={5}>
+        Projects
+      </Heading>
+      <Text fontSize={"lg"} pt={0} mb={4} fontWeight={500} id="robotics">
+        Robotics
+      </Text>
+      <SimpleGrid columns={[1, 1, 2]} spacing={4}>
+        {projects.robotics.map(({ name, year, description, images, imageDescriptions }) => (
+          <Box key={name} h="100%">
+            <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions}></Project>
+          </Box>
+        ))}
+      </SimpleGrid>
+      <Text fontSize={"lg"} pt={5} mb={4} fontWeight={500} id="ai">
+        Artificial Intelligence
+      </Text>
+      <SimpleGrid columns={[1, 1, 2]} spacing={4}>
+        {projects.ai.map(({ name, year, description, images, imageDescriptions }) => (
+          <Box key={name} h="100%">
+            <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions}></Project>
+          </Box>
+        ))}
+      </SimpleGrid>
+      <Text fontSize={"lg"} pt={5} mb={4} fontWeight={500} id="software">
+        Software Development
+      </Text>
+      <SimpleGrid columns={[1, 1, 2]} spacing={4}>
+        {projects.software.map(({ name, year, description, images, imageDescriptions, links }) => (
+          <Box key={name} h="100%">
+            <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions} links={links}></Project>
+          </Box>
+        ))}
+      </SimpleGrid>
+      <Text fontSize={"lg"} pt={5} mb={4} fontWeight={500} id="more">
+        Cloud Development and More
+      </Text>
+      <SimpleGrid columns={[1, 1, 2]} spacing={4}>
+        {projects.cloud.map(({ name, year, description, images, imageDescriptions, links }) => (
+          <Box key={name} h="100%">
+            <Project name={name} year={year} description={description} images={images} idesc={imageDescriptions} links={links}></Project>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
 }
 
 function Project(props: { name: string; year: string; description: Array<String>; images: Array<String>; idesc: Array<string>; links?: Array<string> }) {
-    const { name, year, description, images, idesc, links } = props
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [image, setimage] = useState({
-        src: '',
-        desc: ''
-    })
+  const { name, year, description, images, idesc, links } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [image, setimage] = useState({
+    src: "",
+    desc: "",
+  });
 
-    function handleImageClick(e: React.MouseEvent) {
-        
-        let path : string = e.currentTarget.getAttribute('alt') || ''
-        setimage({src: path, desc: idesc[images.indexOf(path)]})
-        onOpen()
-    }
+  function handleImageClick(e: React.MouseEvent) {
+    let path: string = e.currentTarget.getAttribute("alt") || "";
+    setimage({ src: path, desc: idesc[images.indexOf(path)] });
+    onOpen();
+  }
 
-    return (
-        
-        <Box
-        minH={'260px'}
-        w={'full'}
-        bgColor='gray.100'
-        borderRadius='10px'
-        p={6}
-        h='full'
-        >
-        <Stack spacing={4} direction='column'>
-          <Stack direction={'row'} spacing={0}>
-            <Text fontWeight={600} fontSize='lg'>{name}</Text>
-            <Spacer />
-            {year ? <Badge variant={'solid'} maxH='20px'>{year}</Badge>: null}
-            {/* <Text bgColor={'gray.900'} textColor='white' borderRadius='10px' px='2' py={1}>{year}</Text> */}
-          </Stack>
-        <UnorderedList px={3}>
-        
-        {
-            description.map((desc?: any) => (
-                <ListItem key={desc} fontSize={['0.75em', '0.8em', '0.8em']}>{desc}</ListItem>
-            ))
-        }
-        </UnorderedList>
-        {images.length > 1 ? <Flex flexDirection={'row'} className={'container'} overflowX='scroll'>
-        {
-            images.map((imageUrl: any) => (
-                <Flex key={imageUrl} mr={3} minW='70px' minH={'70px'}>
-                    <IMG src={imageUrl} style={{borderRadius: '20px'}} objectFit='cover' width='70px' height='70px' alt={imageUrl} className='image' onClick={(e) => {
-                    
-                    if (links?.[images.indexOf(imageUrl)]) {
-                        window.open(links?.[images.indexOf(imageUrl)], '_blank')
-                    } else {
-                        handleImageClick(e)
-                    }
-                    }}></IMG>
-                </Flex>       
-            ))
-        }
-        </Flex>: null}
+  return (
+    <Box minH={"260px"} w={"full"} bgColor="gray.100" borderRadius="10px" p={6} h="full">
+      <Stack spacing={4} direction="column">
+        <Stack direction={"row"} spacing={0}>
+          <Text fontWeight={600} fontSize="lg">
+            {name}
+          </Text>
+          <Spacer />
+          {year ? (
+            <Badge variant={"solid"} maxH="20px">
+              {year}
+            </Badge>
+          ) : null}
+          {/* <Text bgColor={'gray.900'} textColor='white' borderRadius='10px' px='2' py={1}>{year}</Text> */}
         </Stack>
-        <Modal isOpen={isOpen} onClose={onClose} allowPinchZoom={true} size='2xl' isCentered={true} motionPreset='slideInBottom'>
+        <UnorderedList px={3}>
+          {description.map((desc?: any) => (
+            <ListItem key={desc} fontSize={["0.75em", "0.8em", "0.8em"]}>
+              {desc}
+            </ListItem>
+          ))}
+        </UnorderedList>
+        {images.length > 1 ? (
+          <Flex flexDirection={"row"} className={"container"} overflowX="scroll">
+            {images.map((imageUrl: any) => (
+              <Flex key={imageUrl} mr={3} minW="70px" minH={"70px"}>
+                <IMG
+                  src={imageUrl}
+                  style={{ borderRadius: "20px" }}
+                  objectFit="cover"
+                  width="70px"
+                  height="70px"
+                  alt={imageUrl}
+                  className="image"
+                  onClick={(e) => {
+                    if (links?.[images.indexOf(imageUrl)]) {
+                      window.open(links?.[images.indexOf(imageUrl)], "_blank");
+                    } else {
+                      handleImageClick(e);
+                    }
+                  }}></IMG>
+              </Flex>
+            ))}
+          </Flex>
+        ) : null}
+      </Stack>
+      <Modal isOpen={isOpen} onClose={onClose} allowPinchZoom={true} size="2xl" isCentered={true} motionPreset="slideInBottom">
         <ModalOverlay />
-        <ModalContent borderRadius={'10px'} bgClip={'content-box'} flexShrink={1}>
-            <Box pt={5}>
+        <ModalContent borderRadius={"10px"} bgClip={"content-box"} flexShrink={1}>
+          <Box pt={5}>
             {/* <IMG quality={50} src={image.src} alt='project image' width='1000px' height='500px' priority objectFit='contain'></IMG>
              */}
-            <ChakraNextImage src={image.src} alt='project image' width={'700px'} objectFit='scale-down' height='500px'></ChakraNextImage>
+            <ChakraNextImage src={image.src} alt="project image" width={"700px"} objectFit="scale-down" height="500px"></ChakraNextImage>
             <Text p={5}>{image.desc}</Text>
-            </Box>
-
-            
+          </Box>
         </ModalContent>
-        
-        </Modal>
-      </Box>
-      
-    )
+      </Modal>
+    </Box>
+  );
 }
